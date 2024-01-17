e7575e5f3ed220fc890bccb0e790201ab13ead4e
miftahullislam <miftahullislamsohan@gmail.com>
2024-01-17 10:28:16 +0600

    Header Section added

--- src/pages/Shared/Header/Header.jsx
+++ src/pages/Shared/Header/Header.jsx
@@ -1,9 +1,15 @@
 import React from 'react';
+import logo from '../../../assets/logo.png'
+import moment from 'moment';
 
 const Header = () => {
     return (
         <div>
-            <h2>This is Header Component</h2>
+            <div className="flex flex-col justify-center items-center mt-5">
+                <img src={logo} alt="" srcset="" />
+                <p className='font-sans text-[#706F6F] text-lg'> Journalism Without Fear or Favour</p>
+                <p className='font-sans text-[#706F6F] text-md font-semibold'>{moment().format("dddd, MMMM D, YYYY")}</p>
+            </div>
         </div>
     );
 };
